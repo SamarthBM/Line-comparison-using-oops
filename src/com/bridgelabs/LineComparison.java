8165ecf0a5b35c54f4042f93995ba910465cb99e
SamarthBM <samarthbm18@gmail.com>
2021-07-07 23:29:58 +0530

    Initial commit

---
+++ src/com/bridgelabs/LineComparison.java
@@ -0,0 +1,18 @@
+/****************************************************************
+ * Purpose :
+ * @author Samarth BM
+
+***************************************************************/
+
+package com.bridgelabs;
+
+
+public class LineComparison {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Line comparison Computation program");
+
+	}
+
+}
+
