0d98cbff648f264e9cffad83e13de0d60a9f988c
SamarthBM <samarthbm18@gmail.com>
2021-07-08 19:12:18 +0530

    Refactored: Used oops concept and added scanner function in main method

--- src/com/bridgelabs/LineComparison.java
+++ src/com/bridgelabs/LineComparison.java
@@ -9,22 +9,16 @@ package com.bridgelabs;
 import java.util.Scanner;
 
 public class LineComparison {
-	
 
 	/* Method to find the length of line. */
 	public static double findLength(int x1, int x2, int y1, int y2) {
-		
+
 		double lenght_of_line = Math.sqrt(Math.pow((x2 - x1), 2) + (Math.pow((y2 - y1), 2)));
-	
 		return lenght_of_line;
 	}
-	
-	// Comparing the two lines.
+
+	/* Method to compare two lines */
 	public static void compareTo(double line1, double line2) {
-//		System.out.println("Enter co-ordinates for first line");
-//		double line1 = findLength();
-//		System.out.println("Enter co-ordinates for second line");
-//		double line2 = findLength();
 
 		if (line1 == line2)
 			System.out.println("Two lines are equal");
@@ -36,9 +30,11 @@ public class LineComparison {
 	}
 
 	public static void main(String[] args) {
-		System.out.println("Welcome to Line comparison Computation program");		
+		System.out.println("Welcome to Line comparison Computation program");
+
+		/* Taking user input for coordinates of two lines */
 		Scanner sc = new Scanner(System.in);
-		
+
 		System.out.println("Enter co-ordinates for first line");
 		System.out.println("Enter value of X1 co-ordinate");
 		int x1 = sc.nextInt();
@@ -48,7 +44,7 @@ public class LineComparison {
 		int y1 = sc.nextInt();
 		System.out.println("Enter value of Y2 co-ordinate");
 		int y2 = sc.nextInt();
-		
+
 		System.out.println("Enter co-ordinates for second line");
 		System.out.println("Enter value of a1 co-ordinate");
 		int a1 = sc.nextInt();
@@ -58,17 +54,16 @@ public class LineComparison {
 		int b1 = sc.nextInt();
 		System.out.println("Enter value of b2 co-ordinate");
 		int b2 = sc.nextInt();
-		
-		double line1 = findLength(x1,x2,y1,y2);
+
+		/* Calling methods to find length and compare two lines. */
+		double line1 = findLength(x1, x2, y1, y2);
 		System.out.println("Length of line 1 is " + line1);
-		
-		double line2 = findLength(a1,a2,b1,b2);
+
+		double line2 = findLength(a1, a2, b1, b2);
 		System.out.println("Length of line 2 is " + line2);
-		
+
 		compareTo(line1, line2);
-		
-		
-		
+		sc.close();
 
 	}
 
